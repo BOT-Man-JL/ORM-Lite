a3c90c6412b132d0dc8a5b77692166921f47b9af
BOT Man JL <g123nn@hotmail.com>
2016-12-09 12:38:49 +0800

    Update Test;

--- test/Test.cpp
+++ test/Test.cpp
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <iostream>
+#include <assert.h>
 
 #include "../src/ORMLite.h"
 using namespace BOT_ORM;
@@ -46,6 +47,14 @@ struct ModelC
 	ORMAP ("ModelC", c_uint, a_int, b_ulong);
 };
 
+struct ModelD
+{
+	int d_int;
+
+	// Inject ORM-Lite into this Class :-)
+	ORMAP ("ModelD", d_int);
+};
+
 int main ()
 {
 	// Open a Connection with *Test.db*
@@ -67,8 +76,11 @@ int main ()
 	initTable (ModelA {});
 	initTable (ModelB {});
 	initTable (ModelC {});
+	initTable (ModelD {});
 
+	//
 	// Case: Not Compile
+	//
 
 	//mapper.CreateTbl (int ());
 	//mapper.DropTbl (int ());
@@ -90,5 +102,45 @@ int main ()
 	//mapper.Query (ModelA {})
 	//	.LeftJoin (int (), Expr { Selectable<int> {}, "" });
 
+	//
+	// Case: Only One Field Mapping
+	//
+
+	ModelA ma;
+	ModelD md;
+	auto field = FieldExtractor { ma, md };
+
+	mapper.Insert (ModelD { 0 });
+	mapper.Insert (ModelD { 0 }, false);
+	mapper.InsertRange (std::list<ModelD> { ModelD { 2 }, ModelD { 3 } });
+	mapper.InsertRange (std::list<ModelD> { ModelD { 2 }, ModelD { 3 } }, false);
+	mapper.Update (ModelD { 0 });
+	mapper.UpdateRange (std::list<ModelD> { ModelD { 2 }, ModelD { 3 } });
+	mapper.Update (ModelD {}, field (md.d_int) = 6, field (md.d_int) == 0);  // 0 -> 6
+	mapper.Delete (ModelD { 1 });
+	mapper.Delete (ModelD {}, field (md.d_int) == 0);  // No such one
+
+	constexpr auto countExpected = 5;
+	constexpr auto firstIdExpected = 2;
+	constexpr auto lastIdExpected = 6;
+	// Expected: 2, 3, 4, 5, 6
+
+	assert (mapper.Query (ModelD {})
+			.Select (Count ()).Value () == countExpected);
+	assert (mapper.Query (ModelD {})
+			.LeftJoin (ModelA {}, field (ma.a_int) == field (md.d_int))
+			.Select (Count ()).Value () == countExpected);
+	mapper.Insert (ModelA {}, false);
+	assert (mapper.Query (ModelD {}).Select (field (md.d_int))
+			.Union (mapper.Query (ModelA {}).Select (field (ma.a_int)))
+			.ToList ().size () == countExpected + 1);
+	assert (mapper.Query (ModelD {})
+			.ToVector ()[countExpected - 1].d_int == lastIdExpected);
+	auto firstTuple = mapper.Query (ModelD {})
+		.Select (field (md.d_int))
+		.ToList ().front ();
+	assert (std::get<0> (firstTuple).Value () == firstIdExpected);
+
+	std::cout << "Test Passing" << std::endl;
 	return 0;
 }
